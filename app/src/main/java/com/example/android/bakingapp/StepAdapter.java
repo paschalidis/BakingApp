3c4d38d0481186004b3adb2fff63d39deb37c077
Pavlos <msf7415012@teilar.gr>
2018-07-11 02:02:08 +0300

    feat: create step adapter and recycler view for steps

---
+++ app/src/main/java/com/example/android/bakingapp/StepAdapter.java
@@ -0,0 +1,92 @@
+package com.example.android.bakingapp;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.bakingapp.models.Step;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StepAdapter extends RecyclerView.Adapter<StepAdapter.StepAdapterViewHolder> {
+
+    private Context mContext;
+    private List<Step> mSteps;
+    private final StepAdapterOnClickHandler mStepClickHandler;
+
+    public interface StepAdapterOnClickHandler {
+        void onClick(Step step);
+    }
+
+    public StepAdapter(Context context, StepAdapterOnClickHandler clickHandler) {
+        mContext = context;
+        mSteps = new ArrayList<>();
+        mStepClickHandler = clickHandler;
+    }
+
+    public class StepAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public final TextView mStepTextView;
+
+        public StepAdapterViewHolder(View itemView) {
+            super(itemView);
+            mStepTextView = (TextView) itemView.findViewById(R.id.recipe_step_text_view);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int adapterPosition = getAdapterPosition();
+            mStepClickHandler.onClick(mSteps.get(adapterPosition));
+        }
+
+        private void bind(String recipeTitle) {
+            mStepTextView.setText(recipeTitle);
+        }
+    }
+
+    @NonNull
+    @Override
+    public StepAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdStepListItem = R.layout.recipe_step_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdStepListItem, parent, shouldAttachToParentImmediately);
+
+        return new StepAdapter.StepAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StepAdapterViewHolder holder, int position) {
+        holder.bind(mSteps.get(position).getShortDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mSteps == null) {
+            return 0;
+        }
+        return mSteps.size();
+    }
+
+    public void setStepData(List<Step> stepData) {
+        mSteps = stepData;
+        notifyDataSetChanged();
+    }
+
+    public void clearData() {
+        mSteps.clear();
+        notifyDataSetChanged();
+    }
+
+    public List<Step> getStepData() {
+        return mSteps;
+    }
+}
--- app/src/main/java/com/example/android/bakingapp/fragments/RecipeFragment.java
+++ app/src/main/java/com/example/android/bakingapp/fragments/RecipeFragment.java
@@ -2,18 +2,24 @@ package com.example.android.bakingapp.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.bakingapp.R;
+import com.example.android.bakingapp.StepAdapter;
 import com.example.android.bakingapp.models.Recipe;
+import com.example.android.bakingapp.models.Step;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class RecipeFragment extends Fragment {
+public class RecipeFragment extends Fragment implements StepAdapter.StepAdapterOnClickHandler {
 
     private Recipe mRecipe;
 
@@ -29,8 +35,20 @@ public class RecipeFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_recipe, container, false);
 
         if (mRecipe != null) {
-            TextView textView = rootView.findViewById(R.id.recipe_name_text_view);
+            TextView textView = rootView.findViewById(R.id.recipe_ingredients_text_view);
             textView.setText(mRecipe.getName());
+
+            RecyclerView recyclerView = rootView.findViewById(R.id.recipe_steps_recycler_view);
+
+            LinearLayoutManager layoutManager = new LinearLayoutManager(rootView.getContext());
+
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setHasFixedSize(true);
+
+            StepAdapter stepAdapter = new StepAdapter(rootView.getContext(), this);
+
+            stepAdapter.setStepData(mRecipe.getSteps());
+            recyclerView.setAdapter(stepAdapter);
         }
 
 
@@ -39,5 +57,10 @@ public class RecipeFragment extends Fragment {
 
     public void setRecipe(Recipe recipe) {
         mRecipe = recipe;
+    }
+
+    @Override
+    public void onClick(Step step) {
+
     }
 }
