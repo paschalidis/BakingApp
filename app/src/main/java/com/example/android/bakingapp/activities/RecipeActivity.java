4de7c9c4012cd721fbaa056f12cc91468d2dd1e1
Pavlos <msf7415012@teilar.gr>
2018-07-12 23:13:35 +0300

    feat: implements master list fragment

--- app/src/main/java/com/example/android/bakingapp/activities/RecipeActivity.java
+++ app/src/main/java/com/example/android/bakingapp/activities/RecipeActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 
 import com.example.android.bakingapp.R;
+import com.example.android.bakingapp.fragments.RecipeDetailFragment;
 import com.example.android.bakingapp.fragments.RecipeFragment;
 import com.example.android.bakingapp.interfaces.StepOnClickHandler;
 import com.example.android.bakingapp.models.Recipe;
@@ -15,6 +16,7 @@ public class RecipeActivity extends AppCompatActivity implements StepOnClickHand
 
     public static final String STEP_ENTITY = "step_entity";
     private Recipe mRecipe;
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,30 @@ public class RecipeActivity extends AppCompatActivity implements StepOnClickHand
             }
         }
 
-        // Only create new fragments when there is no previously saved state
+        if (findViewById(R.id.recipe_activity_linear_layout) != null) {
+            mTwoPane = true;
+
+            // Only create new fragments when there is no previously saved state
+//            if (savedInstanceState == null) {
+//                RecipeFragment recipeFragment = new RecipeFragment();
+//                recipeFragment.setRecipe(mRecipe);
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                fragmentManager.beginTransaction()
+//                        .add(R.id.recipe_fragment_container, recipeFragment)
+//                        .commit();
+
+//                RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
+//                recipeDetailFragment.setStep(mStep);
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                fragmentManager.beginTransaction()
+//                        .add(R.id.recipe_detail_fragment_container, recipeDetailFragment)
+//                        .commit();
+//            }
+
+        } else {
+            mTwoPane = false;
+        }
+
         if (savedInstanceState == null) {
             RecipeFragment recipeFragment = new RecipeFragment();
             recipeFragment.setRecipe(mRecipe);
@@ -37,12 +62,23 @@ public class RecipeActivity extends AppCompatActivity implements StepOnClickHand
                     .add(R.id.recipe_fragment_container, recipeFragment)
                     .commit();
         }
+
     }
 
     @Override
     public void onStepClick(Step step) {
-        Intent intentToStartRecipeDetailActivity = new Intent(this, RecipeDetailActivity.class);
-        intentToStartRecipeDetailActivity.putExtra(STEP_ENTITY, step);
-        startActivity(intentToStartRecipeDetailActivity);
+
+        if (mTwoPane) {
+            RecipeDetailFragment recipeDetailFragment = new RecipeDetailFragment();
+            recipeDetailFragment.setStep(step);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.recipe_detail_fragment_container, recipeDetailFragment)
+                    .commit();
+        } else {
+            Intent intentToStartRecipeDetailActivity = new Intent(this, RecipeDetailActivity.class);
+            intentToStartRecipeDetailActivity.putExtra(STEP_ENTITY, step);
+            startActivity(intentToStartRecipeDetailActivity);
+        }
     }
 }
