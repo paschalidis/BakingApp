e84b42cb0c390727e5a9632678d629bc913c771c
Pavlos <msf7415012@teilar.gr>
2018-07-15 03:08:16 +0300

    feat: adding ExoPlayer

--- app/src/main/java/com/example/android/bakingapp/fragments/RecipeStepFragment.java
+++ app/src/main/java/com/example/android/bakingapp/fragments/RecipeStepFragment.java
@@ -1,6 +1,8 @@
 package com.example.android.bakingapp.fragments;
 
 import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -19,6 +21,17 @@ import com.example.android.bakingapp.R;
 import com.example.android.bakingapp.activities.RecipeActivity;
 import com.example.android.bakingapp.clickHandlers.StepNavigationOnClickHandler;
 import com.example.android.bakingapp.models.Step;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 
 public class RecipeStepFragment extends Fragment {
 
@@ -35,6 +48,8 @@ public class RecipeStepFragment extends Fragment {
     private int mStepIndex;
     private boolean mTwoPane;
     private StepNavigationOnClickHandler mStepNavigationOnClickHandler;
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mSimpleExoPlayerView;
 
     public RecipeStepFragment() {
         mTwoPane = false;
@@ -57,6 +72,10 @@ public class RecipeStepFragment extends Fragment {
         if (mStep != null) {
             // Inflate the layout for this fragment
             rootView = inflater.inflate(R.layout.fragment_recipe_step, container, false);
+
+            mSimpleExoPlayerView = rootView.findViewById(R.id.step_player_view);
+            mSimpleExoPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.drawable.art_cake));
+            initializePlayer(mStep.getVideoUrl());
 
             TextView textView = rootView.findViewById(R.id.recipe_detail_description_text_view);
             textView.setText(mStep.getDescription());
@@ -99,8 +118,49 @@ public class RecipeStepFragment extends Fragment {
             Log.v(TAG, "This fragment has a null Step");
         }
 
+
         return rootView;
     }
+
+    private void initializePlayer(String videoUrl) {
+        if (mExoPlayer == null) {
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+            mSimpleExoPlayerView.setPlayer(mExoPlayer);
+
+            Uri myUri = Uri.parse(videoUrl);
+            String userAgent = "Step Player";
+            MediaSource mediaSource = new ExtractorMediaSource(myUri, new DefaultDataSourceFactory(getContext(),
+                    userAgent), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        releasePlayer();
+    }
+
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        releasePlayer();
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        releasePlayer();
+//    }
 
     public void setStep(Step step) {
         mStep = step;
