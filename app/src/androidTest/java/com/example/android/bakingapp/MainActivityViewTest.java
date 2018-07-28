20f1c7207747f89e5471b72f04b3ee2c6f53f643
Pavlos <msf7415012@teilar.gr>
2018-07-28 15:51:17 +0300

    test: check intent from main activity

--- app/src/androidTest/java/com/example/android/bakingapp/MainActivityViewTest.java
+++ app/src/androidTest/java/com/example/android/bakingapp/MainActivityViewTest.java
@@ -1,9 +1,11 @@
 package com.example.android.bakingapp;
 
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import com.example.android.bakingapp.activities.MainActivity;
+import com.example.android.bakingapp.activities.RecipeActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -14,29 +16,31 @@ import android.support.test.espresso.contrib.RecyclerViewActions;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class MainActivityViewTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule =
             new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void viewContainRecyclerView(){
+    public void viewContainRecyclerView() {
 
         onView(withId(R.id.recipes_recycler_view))
                 .check(matches(isDisplayed()));
     }
 
     @Test
-    public void clickRecyclerViewItem() {
+    public void intentToRecipeActivity() {
+        Intents.init();
         onView(withId(R.id.recipes_recycler_view))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-
-        onView(withId(R.id.recipe_ingredients_text_view))
-                .check(matches(isDisplayed()));
+        intended(hasComponent(RecipeActivity.class.getName()));
+        Intents.release();
     }
 }
